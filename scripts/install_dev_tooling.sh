bc3a3bf15b1d927bed0458dc173e090176aea3f3
Stephen von Takach <steve@place.tech>
2024-05-09 10:24:45 +1000

    feat: fix dev tooling install

--- scripts/install_dev_tooling.sh
+++ scripts/install_dev_tooling.sh
@@ -60,7 +60,7 @@ sudo systemctl restart docker
 
 echo "Docker daemon configuration updated and Docker restarted."
 
-docker buildx bake
 docker run --privileged --rm tonistiigi/binfmt --install all
-docker buildx create --name mybuilder --use
+docker buildx create --name mybuilder --driver docker-container --use
 docker buildx inspect --bootstrap
+docker buildx use mybuilder
